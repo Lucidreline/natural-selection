2b9c6ef551c86e01a4ecc8ba8a457a0ccd537c04
Lucidreline <casta.ma502@gmail.com>
2021-06-19 23:01:47 -0700

    Agent now moves randomly based on its vectors in a closed space.

--- Assets/Agent.cs
+++ Assets/Agent.cs
@@ -6,8 +6,11 @@ public class Agent : MonoBehaviour
 {
 
     private Rigidbody2D rb2D;
-    [SerializeField] static float thrust = 5f;
+    [SerializeField] static float thrust = 0.5f;
+
+    [SerializeField] static float vectorUpdateFreq = 1f;
     [SerializeField] static int vectorQuantity = 5;
+    Vector2 currentVector;
 
     [SerializeField] Vector2[] vectors;
 
@@ -15,12 +18,14 @@ public class Agent : MonoBehaviour
     {
         rb2D = gameObject.GetComponent<Rigidbody2D>();
         vectors = PopulateVector();
+        UpdateVector();
+        StartCoroutine(VectorChanger());
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb2D.AddForce(vectors[0] * thrust);
+        rb2D.AddForce(currentVector * thrust);
     }
 
     static Vector2[] PopulateVector()
@@ -43,5 +48,19 @@ public class Agent : MonoBehaviour
 
         else
             return Random.value;
+    }
+
+    IEnumerator VectorChanger()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(vectorUpdateFreq);
+            UpdateVector();
+        }
+    }
+
+    void UpdateVector()
+    {
+        currentVector = vectors[(int)Random.Range(0f, (float)vectorQuantity)];
     }
 }
